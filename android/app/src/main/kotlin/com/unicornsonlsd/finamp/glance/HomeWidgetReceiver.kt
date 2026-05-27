92358c38938231019bc0e2b59e9fef9779fe9c78
call-me-maple <85463994+call-me-maple@users.noreply.github.com>
2026-05-27 16:59:01 -0600

    add circluar widget

--- android/app/src/main/kotlin/com/unicornsonlsd/finamp/glance/HomeWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-// Remember to set a package in order for home_widget to find the Receiver
-package com.unicornsonlsd.finamp.glance
-
-import es.antonborri.home_widget.HomeWidgetGlanceWidgetReceiver
-
-class HomeWidgetReceiver : HomeWidgetGlanceWidgetReceiver<HomeWidgetGlanceAppWidget>() {
-    override val glanceAppWidget = HomeWidgetGlanceAppWidget()
-}
